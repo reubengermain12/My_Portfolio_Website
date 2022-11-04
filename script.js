d6b13e6d577ebd12974c42f57a533be50562ee76
Germain712 <102153309+Germain712@users.noreply.github.com>
2022-11-04 18:18:46 +0000

    Started adding code to my script.js file - JavaScript

---
+++ script.js
@@ -0,0 +1,11 @@
+/*About Tabs*/
+
+const tabsContainer = document.querySelector(".about-tabs"),
+aboutSection = document.querySelector(".about-section");
+
+tabsContainer.addEventListener("click", (e) =>{
+  if(e.target.classList.contains("tab-item") && !e.target.classList.contains("active")){
+    tabsContainer.querySelector(".active").classList.remove("active");
+    e.target.classList.add("active");
+  }
+});
