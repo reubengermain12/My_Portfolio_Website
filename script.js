9e96c850cb827442dc927dafcc9b296b2e73f419
Germain712 <102153309+Germain712@users.noreply.github.com>
2022-11-04 18:34:44 +0000

    With the code just added, I can now click on the Education & Experience sections, and it will now show the relevant information

--- script.js
+++ script.js
@@ -7,5 +7,8 @@ tabsContainer.addEventListener("click", (e) =>{
   if(e.target.classList.contains("tab-item") && !e.target.classList.contains("active")){
     tabsContainer.querySelector(".active").classList.remove("active");
     e.target.classList.add("active");
+    const target = e.target.getAttribute("data-target");
+    aboutSection.querySelector(".tab-content.active").classList.remove("active");
+    aboutSection.querySelector(target).classList.add("active");
   }
 });
